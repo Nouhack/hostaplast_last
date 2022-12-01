61c18c5a177c25a20d2279abcee17b40326288f3
Nouhack <nouh.saiche@gmail.com>
2022-12-01 11:18:58 +0100

    init8

---
+++ content/aboutus.md
@@ -0,0 +1,8 @@
+---
+title: aboutus
+date: 2019-03-17T19:31:20.591Z
+aboutus: []
+---
+
+Welcome to my awesome page about cats of the internet.
+This page is built with NextJS, and content is managed in Netlify CMS
--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,23 +1,11 @@
 import React from "react";
+import { attributes, react as HomeContent } from "../content/aboutus.md";
 
 export default function aboutus() {
+  let { aboutus } = attributes;
+
   return (
     <div>
-      <header class="text-gray-600 body-font"></header>
-      <section class="text-gray-600 body-font overflow-hidden"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <section class="text-gray-600 body-font"></section>
-      <footer class="text-gray-600 body-font"></footer>
       <footer class="text-gray-600 body-font">
         <div class="bg-gray-100"></div>
       </footer>
@@ -28,19 +16,14 @@ export default function aboutus() {
         <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
           <img
             alt="hero"
-            src="https://dummyimage.com/720x600"
+            src={aboutus.thumbnail}
             class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           />
           <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Qui nous sommes ?
+              {aboutus.title}
             </h1>
-            <p class="mb-8 leading-relaxed">
-              Kickstarter biodiesel roof party wayfarers cold-pressed. Palo
-              santo live-edge tumeric scenester copper mug flexitarian. Prism
-              vice offal plaid everyday carry. Gluten-free chia VHS squid
-              listicle artisan.
-            </p>
+            <p class="mb-8 leading-relaxed">{aboutus.body}</p>
             <form method="get" id="ip4w4"></form>
             <div class="flex"></div>
           </div>
