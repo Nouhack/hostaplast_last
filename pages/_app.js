f832ba22a2254df8662f5e108352dd24963c9d8f
Nouhack <nouh.saiche@gmail.com>
2022-12-01 10:53:19 +0100

    init6

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Script from "next/script";
 import "../styles/globals.css";
 import Header from "../components/header";
@@ -8,9 +7,7 @@ import Footer from "../components/footer";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-      </Head>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
       <Header />
       <Component {...pageProps} />
       <Footer />
