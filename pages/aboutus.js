d3df7579c80736e045516244d090c5a1488762a9
Nouhack <nouh.saiche@gmail.com>
2022-12-01 11:35:50 +0100

    init9

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -2,7 +2,7 @@ import React from "react";
 import { attributes, react as HomeContent } from "../content/aboutus.md";
 
 export default function aboutus() {
-  let { aboutus } = attributes;
+  let { title, thumbnail, body } = attributes;
 
   return (
     <div>
@@ -16,14 +16,14 @@ export default function aboutus() {
         <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
           <img
             alt="hero"
-            src={aboutus.thumbnail}
+            src={thumbnail}
             class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           />
           <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              {aboutus.title}
+              {title}
             </h1>
-            <p class="mb-8 leading-relaxed">{aboutus.body}</p>
+            <p class="mb-8 leading-relaxed">{body}</p>
             <form method="get" id="ip4w4"></form>
             <div class="flex"></div>
           </div>
