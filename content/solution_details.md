a526b1b4af100da1d26c2eb0c87ede3d1c320f4c
Nouhack <nouh.saiche@gmail.com>
2022-12-01 15:45:54 +0100

    init23

---
+++ content/solution_details.md
@@ -0,0 +1,5 @@
+---
+body: fill in the value on your dashboard
+---
+
+this is a markdown type please go the admin dashboard and customize it on your need
--- pages/solution.js
+++ pages/solution.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { attributes, react as HomeContent } from "../content/solution.md";
 
 export default function solution() {
-  return <div>solution</div>;
+  let { body } = attributes;
+  return (
+    <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
+      {body}
+    </div>
+  );
 }
