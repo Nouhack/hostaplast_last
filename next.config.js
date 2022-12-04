0afb0ba77855f80f1dcaaf3bb6676c28c5cf87b5
Nouhack <nouh.saiche@gmail.com>
2022-12-04 14:28:59 +0100

    init24

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const blogPostsFolder = "./content/blogPosts";
+const getPathsForPosts = () => {
+  return fs
+    .readdirSync(blogPostsFolder)
+    .map((blogName) => {
+      const trimmedName = blogName.substring(0, blogName.length - 3);
+      return {
+        [`/blog/post/${trimmedName}`]: {
+          page: "/blog/post/[slug]",
+          query: {
+            slug: trimmedName,
+          },
+        },
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {});
+};
+
 module.exports = {
   images: {
     unoptimized: true,
