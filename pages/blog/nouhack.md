3d90c7dcceaf2a78844616449c40f53813557b16
Nouhack <nouh.saiche@gmail.com>
2022-12-04 14:18:00 +0100

    init24

--- pages/blog/nouhack.md
+++
@@ -1,6 +0,0 @@
----
-name: nouhack
-date: 2022-12-04T12:59:14.134Z
-thumbnail: img/arduino.png
----
-t﻿his is my blog dude and you have to by sure about it
---
+++ pages/readmd.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import Link from "next/link";
+
+const BLOG_POSTS_PATH = "../contant/blogPosts";
+
+const importBlogPosts = async () => {
+  // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
+  // second flag in require.context function is if subdirectories should be searched
+  const markdownFiles = require
+    .context(BLOG_POSTS_PATH, false, /\.md$/)
+    .keys()
+    .map((relativePath) => relativePath.substring(2));
+  return Promise.all(
+    markdownFiles.map(async (path) => {
+      const markdown = await import(`../../content/blogPosts/${path}`);
+      return { ...markdown, slug: path.substring(0, path.length - 3) };
+    })
+  );
+};
+
+const postsList = await importBlogPosts();
+
+export default function readmd() {
+  return (
+    <div className="blog-list">
+      {postsList.map((post) => {
+        return (
+          <Link href={`blog/post/${post.slug}`}>
+            <a>
+              <img src={post.attributes.thumbnail} />
+              <h2>{post.attributes.title}</h2>
+            </a>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
