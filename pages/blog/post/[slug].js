c88c3066261383b020f00357ac2aed2a5f15c756
Nouhack <nouh.saiche@gmail.com>
2022-12-05 14:47:47 +0100

    init24

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { attributes, react as HomeContent } from "../../../content/news.md";
 import ReactMarkdown from "react-markdown";
 
 const Post = () => {
+  const [value, setvalue] = useState("");
+  useEffect(() => {
+    setvalue(news.filter((item) => item.slug == slug)[0].content);
+  }, []);
+
   let { news } = attributes;
 
   const router = useRouter();
   const { slug } = router.query;
-  const specefied_data = news.filter((item) => item.slug == slug);
-  const txt =
-    typeof specefied_data[0].content !== undefined
-      ? specefied_data[0].content
-      : "";
 
-  return <ReactMarkdown>{txt}</ReactMarkdown>;
+  return <ReactMarkdown>{value}</ReactMarkdown>;
 };
 
 export default Post;
