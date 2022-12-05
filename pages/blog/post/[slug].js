32a2e36d91ca788fb7a236d6e64c2209b01e7f72
Nouhack <nouh.saiche@gmail.com>
2022-12-05 09:50:46 +0100

    init24

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function [slug]() {
-  return (
-    <div>[slug]</div>
-  )
+export default function Postitem() {
+  return <div>none</div>;
 }
