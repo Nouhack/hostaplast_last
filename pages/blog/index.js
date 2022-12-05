280bffa57587f71bda16633b577539b137e40bb6
Nouhack <nouh.saiche@gmail.com>
2022-12-05 09:47:13 +0100

    init24

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -31,8 +31,7 @@ export default class Blog extends Component {
       <div className="blog-list">
         {postsList.map((post) => {
           return (
-            //href={`blog/post/${post.slug`}
-            <Link key={index}>
+            <Link key={index} href={`blog/post/${post.slug}`}>
               <a>
                 <img src={post.attributes.thumbnail} />
                 <h2>{post.attributes.title}</h2>
