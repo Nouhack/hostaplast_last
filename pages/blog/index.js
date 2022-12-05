6965e052f90f5acc31eda8a7ec1f682a03487797
Nouhack <nouh.saiche@gmail.com>
2022-12-05 11:58:45 +0100

    init24

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,17 +3,18 @@ import React, { Component } from "react";
 import Link from "next/link";
 
 const BLOG_POSTS_PATH = "../../content/blogPosts";
+const postsPath = path.join(__dirname, "../..", "content/blogPosts");
 
 const importBlogPosts = async () => {
   // https://medium.com/@shawnstern/importing-multiple-markdown-files-into-a-react-component-with-webpack-7548559fce6f
   // second flag in require.context function is if subdirectories should be searched
   const markdownFiles = require
-    .context("../../content/blogPosts", false, /\.md$/)
+    .context(postsPath, false, /\.md$/)
     .keys()
     .map((relativePath) => relativePath.substring(2));
   return Promise.all(
     markdownFiles.map(async (path) => {
-      const markdown = await import(`../../content/blogPosts/${path}`);
+      const markdown = await import(`${postsPath}/${path}`);
       return { ...markdown, slug: path.substring(0, path.length - 3) };
     })
   );
