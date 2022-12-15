f587f56b716797099c8fe6355c29fe7c0bbb3028
Nouhack <nouh.saiche@gmail.com>
2022-12-15 13:46:57 +0100

    fix header for mobile view

--- components/carousel.js
+++ components/carousel.js
@@ -6,28 +6,6 @@ import { attributes, react as HomeContent } from "../content/slider.md";
 export default class NextJsCarousel extends Component {
   render() {
     let { slider } = attributes;
-    const images = [
-      {
-        id: 1,
-        name: "first one",
-        img: "https://images.unsplash.com/photo-1548345680-f5475ea5df84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
-      },
-      {
-        id: 2,
-        name: "second one",
-        img: "https://images.unsplash.com/photo-1555371363-27a37f8e8c46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      },
-      {
-        id: 3,
-        name: "third one",
-        img: "https://images.unsplash.com/photo-1604355231395-bf02775c357f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      },
-      {
-        id: 4,
-        name: "fourth one",
-        img: "https://plus.unsplash.com/premium_photo-1661963463868-241cc572e3ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=719&q=80",
-      },
-    ];
     return (
       <div className="mt-32 w-full h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel>
--- components/header.js
+++ components/header.js
@@ -1,97 +1,158 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { HiOutlineArrowRight } from "react-icons/hi";
+import {
+  HiOutlineArrowRight,
+  HiChartPie,
+  HiShoppingBag,
+  HiInbox,
+  HiUser,
+  HiArrowSmRight,
+  HiTable,
+  HiHome,
+  HiOfficeBuilding,
+  HiQuestionMarkCircle,
+  HiNewspaper,
+  HiMenu,
+} from "react-icons/hi";
+import { MdContactSupport } from "react-icons/md";
 
-import { Navbar, Button, Dropdown } from "flowbite-react";
+import { Navbar, Sidebar, Button, Dropdown } from "flowbite-react";
 import Link from "next/link";
 import { attributes, react as HomeContent } from "../content/metadata.md";
 
+const Slider = ({ state }) => {
+  return (
+    <Sidebar
+      className={`w-screen z-50 ${state ? "block" : "hidden"}`}
+      aria-label="Sidebar with multi-level dropdown example"
+    >
+      <Sidebar.Items
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: "2px",
+          borderBottomStyle: "solid",
+        }}
+      >
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiHome}>
+            Accueil
+          </Sidebar.Item>
+          <Sidebar.Collapse icon={HiOfficeBuilding} label="Notre compagnie">
+            <Sidebar.Item href="#">À propos de nous</Sidebar.Item>
+            <Sidebar.Item href="#">Valeurs</Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Collapse
+            icon={HiQuestionMarkCircle}
+            label="Pour quoi nous ?"
+          >
+            <Sidebar.Item href="#">Avantages</Sidebar.Item>
+            <Sidebar.Item href="#">Solutions</Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Item href="#" icon={HiNewspaper}>
+            Nouvelles
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={MdContactSupport}>
+            Contactez nous
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
 export default function Header() {
+  const [openSlider, setopenSlider] = useState(false);
   let { logo, logolabel, theme } = attributes;
   const router = useRouter();
 
   return (
-    <Navbar
-      className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900  fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
-      fluid={true}
-      rounded={true}
-    >
-      <Link href="/">
-        <Navbar.displayName className="flex flex-row">
-          <img src={logo} className="mr-3 h-10 sm:h-16" alt="Flowbite Logo" />
-          <span
-            className="font-mono self-center whitespace-nowrap font-bold hidden sm:block text-xl font-semibold dark:text-white"
-            style={{
-              fontFamily: "'Unbounded', cursive",
-            }}
-          >
-            {logolabel}
-          </span>
-        </Navbar.displayName>
-      </Link>
-      <div className="flex md:order-2">
-        <Link href="/client" passHref>
-          <Button
-            pill
-            style={{
-              backgroundColor: theme,
-            }}
-          >
-            Espace client
-            <HiOutlineArrowRight className="ml-2 h-5 w-5" />
-          </Button>
-        </Link>
-        <Navbar.Toggle />
-      </div>
-
-      <Navbar.Collapse>
+    <div className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900  fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <Navbar fluid={true} rounded={true}>
         <Link href="/">
-          <Navbar.displayName
-            style={{
-              color: router.pathname === "/" && theme,
-            }}
-          >
-            Accueil
+          <Navbar.displayName className="flex flex-row">
+            <img src={logo} className="mr-3 h-10 sm:h-16" alt="Flowbite Logo" />
+            <span
+              className="font-mono self-center whitespace-nowrap font-bold hidden md:block text-xl font-semibold dark:text-white"
+              style={{
+                fontFamily: "'Unbounded', cursive",
+              }}
+            >
+              {logolabel}
+            </span>
           </Navbar.displayName>
         </Link>
-
-        <Dropdown label="Notre compagnie" inline={true}>
-          <Dropdown.Item>
-            <Link href="/aboutus">À propos de nous</Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link href="/value">Valeurs</Link>
-          </Dropdown.Item>
-        </Dropdown>
-        <Dropdown label="Pourquoi nous ?" inline={true}>
-          <Dropdown.Item>
-            <Link href="/advantages">Avantages</Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link href="/solution">Solution</Link>
-          </Dropdown.Item>
-        </Dropdown>
-
-        <Link href="/news">
-          <Navbar.displayName
-            style={{
-              color: router.pathname === "/news" && theme,
+        <div className="flex md:order-2">
+          <Link href="/client" passHref>
+            <Button
+              pill
+              style={{
+                backgroundColor: theme,
+              }}
+            >
+              Espace client
+              <HiOutlineArrowRight className="ml-2 h-5 w-5" />
+            </Button>
+          </Link>
+          <button
+            className="lg:hidden "
+            onClick={() => {
+              setopenSlider((prev) => !prev);
             }}
           >
-            Nouvelles
-          </Navbar.displayName>
-        </Link>
+            <HiMenu size={30} className="ml-5" />
+          </button>
+        </div>
 
-        <Link href="/contactus">
-          <Navbar.displayName
-            style={{
-              color: router.pathname === "/contactus" && theme,
-            }}
-          >
-            Contactez Nous
-          </Navbar.displayName>
-        </Link>
-      </Navbar.Collapse>
-    </Navbar>
+        <Navbar.Collapse>
+          <Link href="/">
+            <Navbar.displayName
+              style={{
+                color: router.pathname === "/" && theme,
+              }}
+            >
+              Accueil
+            </Navbar.displayName>
+          </Link>
+
+          <Dropdown label="Notre compagnie" inline={true}>
+            <Dropdown.Item>
+              <Link href="/aboutus">À propos de nous</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link href="/value">Valeurs</Link>
+            </Dropdown.Item>
+          </Dropdown>
+          <Dropdown label="Pourquoi nous ?" inline={true}>
+            <Dropdown.Item>
+              <Link href="/advantages">Avantages</Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link href="/solution">Solution</Link>
+            </Dropdown.Item>
+          </Dropdown>
+
+          <Link href="/news">
+            <Navbar.displayName
+              style={{
+                color: router.pathname === "/news" && theme,
+              }}
+            >
+              Nouvelles
+            </Navbar.displayName>
+          </Link>
+
+          <Link href="/contactus">
+            <Navbar.displayName
+              style={{
+                color: router.pathname === "/contactus" && theme,
+              }}
+            >
+              Contactez Nous
+            </Navbar.displayName>
+          </Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <Slider state={openSlider} />
+    </div>
   );
 }
